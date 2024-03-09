faafedf35ef9ce1c7211497e2994e72904300cfd
Itily <aaperlovka@gmail.com>
2024-03-09 11:30:17 +0500

    Создано консольное приложение. Цикл работает до выхода из программы по решению пользователя. Реализован вывод сообщений об ошибках ввода.

--- Lab3.ConsoleApp/Program.cs
+++ Lab3.ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Calc
+{
+    public static void Main()
+    {
+        ConsoleKeyInfo btn;
+
+        Console.WriteLine("Здравствуйте! Это программа для расчета площади кругового сектора");
+        Console.WriteLine("Пожалуйста, следуйте предложенной инструкции!");
+        Console.WriteLine("Для выхода из программы после расчетов нажмите клавишу Esc.");
+        Console.WriteLine("Для продолжения расчетов нажмите клавишу Enter для появления инструкции.");
+
+        do
+        {
+            Console.WriteLine("Введите радиус кругового сектора");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите центральный угол кругового сектора");
+            double alfa = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            try
+            {
+                result = Lab3.Lib.Circular_Sector.Calc(r, alfa);
+                Console.WriteLine("Площадь кругового сектора:");
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            btn = Console.ReadKey();
+        }
+        while (!(btn.Key == ConsoleKey.Escape));
+    }
+}
